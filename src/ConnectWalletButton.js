18304925d0df243e16baedbcf100c256de994460
yiranlandtour <yiranlandtour@163.com>
2023-12-23 21:41:58 +0800

    add wallet

---
+++ src/ConnectWalletButton.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Wallet } from "@viaprotocol/web3-wallets";
+
+function ConnectWalletButton() {
+  const handleConnectWallet = async () => {
+    try {
+      const wallet = new Wallet();
+      await wallet.connect();
+      alert("Connected to wallet successfully!");
+      // 连接成功后，可以在这里执行其他逻辑
+    } catch (error) {
+      console.error("Error connecting to wallet:", error);
+      alert("Error connecting to wallet. Please try again.");
+    }
+  };
+
+  return (
+    <button onClick={handleConnectWallet}>Connect Wallet</button>
+  );
+}
+
+export default ConnectWalletButton;
