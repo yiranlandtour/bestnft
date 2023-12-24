9420efdaf36cd04c54fcd48a50cc2de74961fad8
yiranlandtour <yiranlandtour@163.com>
2023-12-24 12:49:40 +0800

    bug fix

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Button /> 
+        <ButtonComponent />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
