82067454a31d41cc957c501673b3fdf28ca81f8d
yiranlandtour <yiranlandtour@163.com>
2023-12-25 10:21:01 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Button /> 
         <img src={logo} className="App-logo" alt="logo" />
-        <Button /> 
+        
         
         <p>
           Edit <code>src/App.js</code> and save to reload.
